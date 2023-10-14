56399430768cafc20791eb816c7375720034934a
Tarun Sheoran <118544285+tarunsheoran@users.noreply.github.com>
2023-10-14 21:37:27 +0530

    Add files via upload

---
+++ Typecasting.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int x =68;
+	float y =88.6;
+	cout<<"The value of x = "<<(float)x<<endl;
+	cout<<"The value of y = "<<(int)y<<endl;
+return 0;
+}
